e49edd32544dd1a74cca87d7640cfdc40903af1a
kevinresol <kevinresol@gmail.com>
2016-06-15 12:32:10 +0800

    Add Loggers

---
+++ src/travix/loggers/FlashLogger.hx
@@ -0,0 +1,18 @@
+package travix.loggers;
+
+import flash.system.System;
+
+class FlashLogger {
+  static function print(s:String) {
+    
+  }
+  
+  static function println(s:String) {
+    
+  }
+  
+  static function exit(code:Bool) {
+    
+  }
+  
+}
