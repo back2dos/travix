138c77d55059d8f263d25b9d48c857af03b13404
kevinresol <kevinresol@gmail.com>
2016-06-17 12:16:24 +0800

    Update default test entry point

--- src/travix/default.hx
+++ src/travix/default.hx
@@ -4,18 +4,7 @@ class $name {
 
   static function main() {
     trace('it works');
-    #if flash
-      flash.system.System.exit(0);//Don't forget to exit on flash!
-    #end
-    
-    #if (travix && js)
-      // travix run js tests in phantomjs, so we need to exit properly
-      var callPhantom = untyped js.Browser.window.callPhantom;
-      callPhantom({
-        cmd: 'travix:exit',
-        exitCode: 0,
-      });
-    #end
+    travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
   }
   
 }
