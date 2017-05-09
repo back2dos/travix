2d2f2bda369a709be5f185782cc3463d9dfc4b01
Kevin Leung <kevinresol@gmail.com>
2017-05-09 10:32:44 +0800

    hmmm

--- src/travix/commands/JsCommand.hx
+++ src/travix/commands/JsCommand.hx
@@ -4,7 +4,13 @@ using sys.io.File;
 using sys.FileSystem;
 
 class JsCommand extends Command {
+  
+  static var PHANTOM_JS = "phantomjs-2.1.1-linux-x86_64";
+  
   override function execute() {
+    
+    var phantomjs = 'phantomjs';
+    
     if(Travix.isTravis) {
       if(Travix.isMac) {
         aptGet('phantomjs');
@@ -16,16 +22,10 @@ class JsCommand extends Command {
           for(dep in ['build-essential', 'chrpath', 'libssl-dev', 'libxft-dev', 'libfreetype6', 'libfreetype6-dev', 'libfontconfig1', 'libfontconfig1-dev'])
             aptGet(dep);
 
-          var PHANTOM_JS = "phantomjs-2.1.1-linux-x86_64";
           exec('wget', ['https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2']);
           exec('tar', ['xvjf', '$PHANTOM_JS.tar.bz2']);
           
-          exec('which phantomjs');
-          exec('sudo', ['ln', '-sf', '$PHANTOM_JS/bin/phantomjs', '/usr/local/phantomjs/bin']);
-          exec('sudo', ['ln', '-sf', '$PHANTOM_JS/bin/phantomjs', '/usr/local/phantomjs/phantomjs']);
-          exec('sudo', ['ln', '-sf', '$PHANTOM_JS/bin/phantomjs', '/usr/local/bin']);
-          exec('which phantomjs');
-          exec('$PHANTOM_JS/bin/phantomjs -v');
+          phantomjs = '$PHANTOM_JS/bin/phantomjs';
         });
       }
     }
@@ -35,8 +35,8 @@ class JsCommand extends Command {
       if(!index.exists()) index.saveContent(defaultIndexHtml());
       var runPhantom = 'bin/js/runPhantom.js';
       if(!runPhantom.exists()) runPhantom.saveContent(defaultPhantomScript());
-      exec('phantomjs', ['-v']);
-      exec('phantomjs', ['--web-security=no', runPhantom]);
+      exec(phantomjs, ['-v']);
+      exec(phantomjs, ['--web-security=no', runPhantom]);
     });
   }
   
