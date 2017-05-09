2d8d8dc0ba2d94a49d3e33beed38a8e4731574c2
Kevin Leung <kevinresol@gmail.com>
2017-05-09 09:44:01 +0800

    higher priority

--- src/travix/commands/JsCommand.hx
+++ src/travix/commands/JsCommand.hx
@@ -18,7 +18,7 @@ class JsCommand extends Command {
         var PHANTOM_JS = "phantomjs-2.1.1-linux-x86_64";
         exec('wget', ['https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2']);
         exec('tar', ['xvjf', '$PHANTOM_JS.tar.bz2']);
-        exec('export PATH=$$PATH:$$PWD/$PHANTOM_JS/bin/');
+        exec('export PATH=$$PWD/$PHANTOM_JS/bin/:$$PATH');
       }
     }
     
