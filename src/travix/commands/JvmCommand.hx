25879d3dd1ce8fec90265f607279ea61363fcec1
Kevin Leung <kevinresol@gmail.com>
2026-06-28 21:47:07 +1000

    jvm support

---
+++ src/travix/commands/JvmCommand.hx
@@ -0,0 +1,20 @@
+package travix.commands;
+
+import tink.cli.Rest;
+
+using sys.FileSystem;
+
+class JvmCommand extends Command {
+	public function install() {}
+
+	public function buildAndRun(rest:Rest<String>) {
+		var main = Travix.getMainClassLocalName();
+
+		installLib('hxjava');
+
+		build('jvm', ['-jvm', 'bin/jvm'].concat(rest), function() {
+			var outputFile = main + (isDebugBuild(rest) ? '-Debug' : '');
+			exec('java', ['-jar', 'bin/java/$outputFile.jar']);
+		});
+	}
+}
