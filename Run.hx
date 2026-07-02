b789484e88c118550b034605abdedef9b626cd49
Kevin Leung <kevinresol@gmail.com>
2026-07-02 14:54:57 +1000

    skip deprecation warnings

--- Run.hx
+++ Run.hx
@@ -1,6 +1,6 @@
 class Run {
 	static function main() {
-		final code = Sys.command('haxe -lib tink_cli -lib hx3compat -cp src --run travix.Travix ${Sys.args().map(v -> '"$v"').join(' ')}');
+		final code = Sys.command('haxe -lib tink_cli -lib hx3compat -cp src -D no-deprecation-warnings --run travix.Travix ${Sys.args().map(v -> '"$v"').join(' ')}');
 		Sys.exit(code);
 	}
 }
